ac2575f96fb2479b771861b89758b2a512160fd1
Temirlan <ltemirlan2003@gmail.com>
2024-05-07 19:50:14 +0600

    Clear home page

--- src/pages/public/home/ui/home.tsx
+++ src/pages/public/home/ui/home.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useTranslations } from "next-intl";
-
 export default function Page() {
-  const t = useTranslations();
-  return <div>{t("Home")}</div>;
+  return <div>Главная</div>;
 }
