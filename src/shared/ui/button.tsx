fd52d94000cef6a26e2e76ae09fce578c0f7ac08
Temirlan <ltemirlan2003@gmail.com>
2024-05-10 14:19:52 +0600

    Update ui paddings

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -26,7 +26,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-auto rounded-md py-[12px] px-[16px]",
+        lg: "h-auto rounded-md py-[14px] px-[16px]",
         icon: "h-9 w-9",
       },
     },
