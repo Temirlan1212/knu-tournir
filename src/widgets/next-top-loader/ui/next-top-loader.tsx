c89c3696c4b07d942b6d1bb92038d212739003a3
Temirlan <ltemirlan2003@gmail.com>
2024-05-10 14:55:08 +0600

    Add check is next-top-loader is fully loaded

--- src/widgets/next-top-loader/ui/next-top-loader.tsx
+++ src/widgets/next-top-loader/ui/next-top-loader.tsx
@@ -3,10 +3,13 @@ import NextTopLoader from "nextjs-toploader";
 import { useTheme } from "next-themes";
 import { selectThemeConfig, useThemeConfig } from "@/features/theme";
 import { DefaultThemes } from "@/shared/constants/theme";
+import { useIsMounted } from "@/shared/lib/hooks/useIsMounted";
 export default function Page() {
+  const { isMounted } = useIsMounted();
   const { theme } = useTheme();
   const mode = useThemeConfig(selectThemeConfig);
   const dark = mode.dark || DefaultThemes.dark;
+  if (!isMounted) return null;
 
   return <NextTopLoader color={theme === dark ? "red" : "black"} />;
 }
