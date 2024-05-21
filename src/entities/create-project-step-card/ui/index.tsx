f36d377e29efb4efa15c0c7b74575dbddd6bcc40
Temirlan <ltemirlan2003@gmail.com>
2024-05-21 19:38:20 +0600

    Modify create-project-step-card

--- src/entities/create-project-step-card/ui/index.tsx
+++ src/entities/create-project-step-card/ui/index.tsx
@@ -14,7 +14,7 @@ export default function MainComponent({
   children,
 }: PropsWithChildren<MainComponentProps>) {
   return (
-    <Card className="relative p-[10px] min-h-[480px] max-h-[90dvh] border-none max-w-[500px] shadow-none overflow-y-auto flex flex-col items-center">
+    <Card className="relative p-[10px] max-h-[90dvh] max-w-[500px] border-none shadow-none overflow-y-auto flex flex-col items-center m-auto">
       <CardHeader className="px-0 pt-0 flex items-center">
         <CardTitle className="text-2xl text-center">{title}</CardTitle>
         <CardDescription className="text-center max-w-[370px]">
