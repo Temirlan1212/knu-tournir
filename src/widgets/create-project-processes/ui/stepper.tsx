ce5b0c778db2112f2cb1dacdd36c691c29ecf7da
Temirlan <ltemirlan2003@gmail.com>
2024-05-21 21:15:17 +0600

    Modify create-project-processes stepper

--- src/widgets/create-project-processes/ui/stepper.tsx
+++ src/widgets/create-project-processes/ui/stepper.tsx
@@ -3,7 +3,7 @@ import { Step, Stepper } from "@/ui/stepper";
 import { steps } from "../model/constants/steps";
 import Content from "./stepper-content";
 
-export default function StepperDemo() {
+export default function StepperComponent() {
   return (
     <div className="flex w-full flex-col gap-4">
       <Stepper
