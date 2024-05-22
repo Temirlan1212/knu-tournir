97207d62839e336c449771381e1f45f996e7b399
Temirlan <ltemirlan2003@gmail.com>
2024-05-22 09:50:30 +0600

    Modify stepper

--- src/shared/ui/stepper/index.tsx
+++ src/shared/ui/stepper/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { StepperProvider } from "./context";
 import { Step } from "./step";
 import type { StepItem, StepProps, StepperProps } from "./types";
 import { useMediaQuery } from "./use-media-query";
@@ -14,115 +13,84 @@ const VARIABLE_SIZES = {
   lg: "44px",
 };
 
-const Stepper = React.forwardRef<HTMLDivElement, StepperProps>(
-  (props, ref: React.Ref<HTMLDivElement>) => {
-    const {
-      className,
-      children,
-      orientation: orientationProp,
-      state,
-      responsive,
-      checkIcon,
-      errorIcon,
-      onClickStep,
-      mobileBreakpoint,
-      expandVerticalSteps = false,
-      initialStep = 0,
-      size,
-      steps,
-      variant,
-      styles,
-      variables,
-      scrollTracking = false,
-      ...rest
-    } = props;
+const Steps = React.forwardRef<
+  HTMLDivElement,
+  Pick<StepperProps, "className" | "children">
+>((props, ref: React.Ref<HTMLDivElement>) => {
+  const { className, children } = props;
+  const {
+    orientation: orientationProp,
+    state,
+    responsive,
+    checkIcon,
+    errorIcon,
+    onClickStep,
+    mobileBreakpoint,
+    expandVerticalSteps = false,
+    initialStep = 0,
+    size,
+    steps,
+    variant,
+    styles,
+    variables,
+    scrollTracking = false,
+    ...rest
+  } = useStepper();
 
-    const childArr = React.Children.toArray(children);
+  const childArr = React.Children.toArray(children);
 
-    const items = [] as React.ReactElement[];
+  const items = [] as React.ReactElement[];
 
-    const footer = childArr.map((child, _index) => {
-      if (!React.isValidElement(child)) {
-        throw new Error("Stepper children must be valid React elements.");
-      }
-      if (child.type === Step) {
-        items.push(child);
-        return null;
-      }
+  const footer = childArr.map((child, _index) => {
+    if (!React.isValidElement(child)) {
+      throw new Error("Stepper children must be valid React elements.");
+    }
+    if (child.type === Step) {
+      items.push(child);
+      return null;
+    }
 
-      return child;
-    });
+    return child;
+  });
 
-    const stepCount = items.length;
+  const stepCount = items.length;
 
-    const isMobile = useMediaQuery(
-      `(max-width: ${mobileBreakpoint || "768px"})`
-    );
+  const isMobile = useMediaQuery(`(max-width: ${mobileBreakpoint || "768px"})`);
 
-    const clickable = !!onClickStep;
+  const orientation = isMobile && responsive ? "vertical" : orientationProp;
 
-    const orientation = isMobile && responsive ? "vertical" : orientationProp;
-
-    const isVertical = orientation === "vertical";
-
-    return (
-      <StepperProvider
-        value={{
-          initialStep,
-          orientation,
-          state,
-          size,
-          responsive,
-          checkIcon,
-          errorIcon,
-          onClickStep,
-          clickable,
-          stepCount,
-          isVertical,
-          variant: variant || "circle",
-          expandVerticalSteps,
-          steps,
-          scrollTracking,
-          styles,
-        }}
-      >
-        <div
-          ref={ref}
-          className={cn(
-            "stepper__main-container",
-            "flex w-full flex-wrap",
-            stepCount === 1 ? "justify-end" : "justify-between",
-            orientation === "vertical" ? "flex-col" : "flex-row",
-            variant === "line" && orientation === "horizontal" && "gap-4",
-            className,
-            styles?.["main-container"]
-          )}
-          style={
-            {
-              "--step-icon-size":
-                variables?.["--step-icon-size"] ||
-                `${VARIABLE_SIZES[size || "md"]}`,
-              "--step-gap": variables?.["--step-gap"] || "8px",
-            } as React.CSSProperties
-          }
-          {...rest}
-        >
-          <VerticalContent>{items}</VerticalContent>
-        </div>
-        {orientation === "horizontal" && (
-          <HorizontalContent>{items}</HorizontalContent>
+  return (
+    <>
+      <div
+        ref={ref}
+        className={cn(
+          "stepper__main-container",
+          "flex w-full flex-wrap",
+          stepCount === 1 ? "justify-end" : "justify-between",
+          orientation === "vertical" ? "flex-col" : "flex-row",
+          variant === "line" && orientation === "horizontal" && "gap-4",
+          className,
+          styles?.["main-container"]
         )}
-        {footer}
-      </StepperProvider>
-    );
-  }
-);
-
-Stepper.defaultProps = {
-  size: "md",
-  orientation: "horizontal",
-  responsive: true,
-};
+        style={
+          {
+            "--step-icon-size":
+              variables?.["--step-icon-size"] ||
+              `${VARIABLE_SIZES[size || "md"]}`,
+            "--step-gap": variables?.["--step-gap"] || "8px",
+          } as React.CSSProperties
+        }
+        {...rest}
+      >
+        <VerticalContent>{items}</VerticalContent>
+      </div>
+      {orientation === "horizontal" && (
+        <HorizontalContent>{items}</HorizontalContent>
+      )}
+      {footer}
+    </>
+  );
+});
 
 const VerticalContent = ({ children }: { children: React.ReactNode }) => {
   const { activeStep } = useStepper();
@@ -179,8 +147,8 @@ const HorizontalContent = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-Stepper.displayName = "Stepper";
+Steps.displayName = "Steps";
 Step.displayName = "Step";
 
-export { Stepper, Step, useStepper };
+export { Steps, Step, useStepper };
 export type { StepProps, StepperProps, StepItem };
--- src/widgets/create-project-processes/ui/stepper.tsx
+++ src/widgets/create-project-processes/ui/stepper.tsx
@@ -1,23 +1,58 @@
+// "use client";
+// import { Step, Stepper } from "@/ui/stepper";
+// import { steps } from "../model/constants/steps";
+// import Content from "./stepper-content";
+
+// export default function StepperComponent() {
+//   return (
+//     <div className="flex w-full flex-col gap-4">
+//       <Stepper
+//         initialStep={0}
+//         steps={steps}
+//         responsive={false}
+//         variant="circle-column"
+//         orientation="horizontal"
+//         onClickStep={(step) => {
+//           console.log(step);
+//         }}
+//       >
+//         {steps.map((stepProps, index) => (
+//           <Step key={index} {...stepProps}></Step>
+//         ))}
+
+//         <Content />
+//       </Stepper>
+//     </div>
+//   );
+// }
+
 "use client";
-import { Step, Stepper } from "@/ui/stepper";
+import { Step, Steps } from "@/ui/stepper";
 import { steps } from "../model/constants/steps";
 import Content from "./stepper-content";
+import { StepperProvider } from "@/shared/ui/stepper/context";
 
 export default function StepperComponent() {
   return (
     <div className="flex w-full flex-col gap-4">
-      <Stepper
-        initialStep={0}
-        steps={steps}
-        responsive={false}
-        variant="line"
-        orientation="horizontal"
+      <StepperProvider
+        value={{
+          steps: steps,
+          initialStep: 0,
+          responsive: false,
+          variant: "circle-column",
+          orientation: "horizontal",
+          clickable: true,
+        }}
       >
-        {steps.map((stepProps, index) => (
-          <Step key={index} {...stepProps}></Step>
-        ))}
+        <Steps>
+          {steps.map((stepProps, index) => (
+            <Step key={index} {...stepProps}></Step>
+          ))}
+        </Steps>
+
         <Content />
-      </Stepper>
+      </StepperProvider>
     </div>
   );
 }
