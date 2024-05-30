47aafd687cf809d885860ab6b5332d722c4271b3
Temirlan <ltemirlan2003@gmail.com>
2024-05-30 13:18:04 +0600

    Fixbug build fails

--- src/entities/checkbox-with-label-accordion/index.ts
+++ src/entities/checkbox-with-label-accordion/index.ts
@@ -1,5 +1,6 @@
+import { DataProps, ValuesProps } from "@/shared/ui/checkbox";
 export { default as CheckboxWithLabelAccordionItem } from "./ui/checkbox-with-label-accordion-item";
 export type {
   ValuesProps as CheckboxWithLabelAccordionItemValuesProps,
   DataProps as CheckboxWithLabelAccordionItemDataProps,
-} from "./ui/checkbox-with-label-accordion-item";
+};
