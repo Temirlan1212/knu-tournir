e34c4f6d36cf7198dcb96371fe8b97b9998a7108
Temirlan <ltemirlan2003@gmail.com>
2024-06-01 11:17:02 +0600

    add: middlewares to auth pages

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
+import { options } from "@/app/api/auth/[...nextauth]/options";
 import { LoginByEmailCard } from "@/features/auth";
+import { paths } from "@/shared/routing";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await getServerSession(options);
+  if (session != null) return redirect(paths.dashboard.manage);
   return <LoginByEmailCard />;
 }
--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -1,5 +1,11 @@
 import RegistrationPage from "@/pages/public/registration";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { paths } from "@/shared/routing";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+  const session = await getServerSession(options);
+  if (session != null) return redirect(paths.dashboard.manage);
   return <RegistrationPage />;
 }
